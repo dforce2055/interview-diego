953c8b2236126c622385fd7954c1ab4cdf244804
Diego Pérez <dperez2055@gmail.com>
2024-11-08 22:15:54 +0000

    Adding Button component

---
+++ vue-project/src/components/ui/Button/Button.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+
+enum BUTTON_TYPE {
+  SAVE = 'SAVE',
+  DELETE = 'DELETE'
+}
+
+const props = defineProps({
+  text: {
+    type: String,
+    default: ''
+  },
+  buttonType: {
+    type: String as () => BUTTON_TYPE,
+    default: BUTTON_TYPE.SAVE 
+  }
+})
+
+const emit = defineEmits(['save', 'delete'])
+
+const onClick = () => {
+  if (props.buttonType === BUTTON_TYPE.SAVE) {
+    emit('save')
+  }
+  if (props.buttonType === BUTTON_TYPE.DELETE) {
+    emit('delete')
+  } 
+}
+
+</script>
+
+<template>
+  <button 
+    :class="[props.buttonType === BUTTON_TYPE.SAVE ? 'bg-red-500 text-white font-bold' : '']"
+    @click=onClick
+    >
+    {{ props.text }}
+  </button>
+</template>
